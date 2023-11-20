a3936e40f5bd08eed9fe15e16b698bfcb52ac949
Vaggelis Yfantis <me@octoper.me>
2023-11-20 02:00:48 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 import type { Plugin } from 'vite'
 import entryServerTemplate from './entry.server.template.txt'
 import MagicString from 'magic-string';
+import path from 'node:path';
 
 export interface VitePluginOptions {
   serverInput?: string;
 }
 
 function VitePluginRemixCfWorkers(): Plugin {
+  let remixVitePlugin;
+  let rootDirectory;
   return {
     name: `vite-plugin-remix-cf-workers`,
     apply: 'build',
     enforce: 'pre',
     configResolved(config) {
-      const remixVitePlugin = config.plugins.find(
+      remixVitePlugin = config.plugins.find(
         (p) => p.name === 'remix'
       );
 
@@ -22,7 +25,16 @@ function VitePluginRemixCfWorkers(): Plugin {
         );
       }
     },
-    config() {
+    config(_viteUserConfig, viteConfigEnv) {
+      const viteCommand = viteConfigEnv.command;
+      rootDirectory = _viteUserConfig.root ?? process.env.REMIX_ROOT ?? process.cwd();
+
+      let isSsrBuild =
+        "ssrBuild" in viteConfigEnv &&
+        typeof viteConfigEnv.ssrBuild === "boolean"
+          ? viteConfigEnv.ssrBuild // Vite v4 back compat
+          : viteConfigEnv.isSsrBuild;
+
       return {
         resolve: {
           conditions: ['webworker', 'worker'],
@@ -30,6 +42,21 @@ function VitePluginRemixCfWorkers(): Plugin {
         ssr: {
           target: 'webworker',
         },
+        ...(viteCommand === "build" && {
+          build: {
+            ...(isSsrBuild && {
+              rollupOptions: {
+                input: [
+                  // @ts-ignore
+                  path.resolve(rootDirectory, 'server.ts')
+                ],
+                output: {
+                  entryFileNames: '[name].js',
+                }
+              },
+            }),
+          }
+        }),
       };
     },
     async transform(_src, id) {
